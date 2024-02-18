c15a2df7545507b99231d060ecbc849feb7c4d7f
MjFarzaneh <m.farzane@ernyka.com>
2024-02-18 13:48:16 +0330

    Create the project structure.

---
+++ src/main/java/com/training/unittest/TrainingUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.training.unittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingUnitTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/unittest/TrainingUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.unittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
